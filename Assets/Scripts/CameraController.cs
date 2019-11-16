f205366ae14b494954d38b7be6293d78e43853b8
Sarthak18CSU248 <57385445+Sarthak18CSU248@users.noreply.github.com>
2019-11-16 12:14:37 +0530

    Second commit

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public GameObject player;
+    public float cameraZpos = -10.0f;
+    public float cameraXOffset = 5f;
+    public float cameraYOffset = 1f;
+
+    public  float HorizontalSpeed = 2.0f;
+
+    public  float VerticalSpeed = 2.0f;
+
+    private Transform _camera;
+    private PlayerController _playerController;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _playerController = player.GetComponent<PlayerController>();
+        _camera = Camera.main.transform;
+        _camera.position = new Vector3(player.transform.position.x + cameraXOffset, player.transform.position.y + cameraYOffset, player.transform.position.z + cameraZpos);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (_playerController.isFacingRight)
+        {
+            _camera.position = new Vector3(Mathf.Lerp(_camera.position.x, player.transform.position.x + cameraXOffset, HorizontalSpeed * Time.deltaTime),
+                Mathf.Lerp(_camera.position.y, player.transform.position.y + cameraYOffset, VerticalSpeed * Time.deltaTime),
+                cameraZpos);
+        }
+        else
+        {
+            _camera.position = new Vector3(Mathf.Lerp(_camera.position.x, player.transform.position.x - cameraXOffset, HorizontalSpeed * Time.deltaTime),
+                Mathf.Lerp(_camera.position.y, player.transform.position.y + cameraYOffset, VerticalSpeed * Time.deltaTime),
+                cameraZpos);
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Prime31;
+
+public class PlayerController : MonoBehaviour
+{
+    public CharacterController2D.CharacterCollisionState2D flags;
+    public float walkSpeed = 6.0f;
+    public float jumpSpeed =9.0f;
+    public float gravity = 20.0f;
+    public bool isGrounded;
+    public bool isJumping;
+    public bool isFacingRight;
+
+    private Vector3 _moveDirection = Vector3.zero;
+    private CharacterController2D _characterController;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _characterController = GetComponent<CharacterController2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        _moveDirection.x = Input.GetAxis("Horizontal");
+        _moveDirection.x *= walkSpeed;
+
+        if (isGrounded)
+        {
+            _moveDirection.y = 0;
+            isJumping = false;
+
+            if (_moveDirection.x < 0 )
+            {
+                transform.eulerAngles = new Vector3(0, 180, 0);
+                isFacingRight = false;
+            }
+
+            else if (_moveDirection.x > 0)
+            {
+                transform.eulerAngles = new Vector3(0, 0, 0);
+                isFacingRight = true;
+            }
+            if (Input.GetButtonDown("Jump"))
+            {
+
+                _moveDirection.y = jumpSpeed;
+                isJumping = true;
+            }
+        }
+        else
+        {
+            if (Input.GetButtonUp("Jump"))
+            {
+                if (_moveDirection.y >0)
+                {
+                    _moveDirection.y = _moveDirection.y * 0.5f;
+                }
+            }
+        }
+
+        _moveDirection.y -= gravity * Time.deltaTime;
+        _characterController.move(_moveDirection * Time.deltaTime);
+        flags = _characterController.collisionState;
+
+        isGrounded = flags.below;
+        if(flags.above)
+        {
+            _moveDirection.y -= gravity * Time.deltaTime; 
+        }
+    }
+}
